9d6a45929183389dd8feda4efa6188d447f0b667
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-24 09:43:40 -0300

    Agregado multer y post para obtener metadata del archivo subido

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "./public/data/uploads/" });
+const cors = require("cors");
+require("dotenv").config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { file } = req;
+  const { originalname: name, mimetype: type, size } = file;
 
-
+  res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
